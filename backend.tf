3f49c49774eeaceaa8c648203fe1678344fdca3d
TARUN CHAND I <51223144+Tarun-Chand-Illapu@users.noreply.github.com>
2024-07-26 18:49:51 -0400

    backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tarun-chand-terraform-statefile-backend"
+    bucket = "tarun-tfbackend"
     key = "terraform/terraform.tfstate"
     region = "us-east-1"
     encrypt = true
